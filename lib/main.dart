41513b7808954a92eaef5ff8e0eaa804e13dfc58
Skredder <taylor_juve@outlook.com>
2023-07-10 12:14:11 -0700

    gets nearest languages without repeats in order

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class MyHomePage extends StatefulWidget {
 
 // TODO: first sound using device language
 // TODO: following sounds using location
+// TODO: general null safety
 // Plan: get location; forEach coordinate add pair to priorityQueue;
 // play the first pair's audiofile then the next until empty
 class _MyHomePageState extends State<MyHomePage> {
@@ -117,9 +118,15 @@ class _MyHomePageState extends State<MyHomePage> {
   // Stores GPS coordinates of capital of country using the list of official
   // languages (supported languages only)
   static const Map<Coordinate, List<String>> coordinateToLanguages = {
-    Coordinate(-25.746111, 28.188056): ['Afrikaans'], // South Africa
-    Coordinate(9.016667, 38.75): ['Amharic'], // Ethiopia
-    Coordinate(36.7, 3.216667): ['Arabic'], // Algeria
+    Coordinate(-25.746111, 28.188056): [
+      'zulu',
+      'afrikaans',
+      'english',
+    ], // South Africa
+    Coordinate(9.016667, 38.75): ['amharic', 'somali'], // Ethiopia
+    Coordinate(36.7, 3.216667): ['arabic'], // Algeria
+    Coordinate(26.216667, 50.583333): ['arabic'], // Bahrain
+    Coordinate(-11.683333, 43.266667): ['arabic', 'french'], // Comoros
   };
 
   final AudioPlayer player = AudioPlayer();
@@ -135,7 +142,8 @@ class _MyHomePageState extends State<MyHomePage> {
     _determineLocation().then((location) => getAudioFilesInOrder(location));
   }
 
-  // Sample code found at 'https://pub.dev/packages/geolocator'
+  // Sample code found at 'https://pub.dev/packages/geolocator''
+  // TODO: Handle errors
   Future<Position> _determineLocation() async {
     bool serviceEnabled;
     LocationPermission permission;
@@ -174,9 +182,9 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   // TODO: work in progress!
-  List<String> getAudioFilesInOrder(Position location) {
+  List<String?> getAudioFilesInOrder(Position location) {
+    // add the distance to each capital and the languages spoken there
     var nearestLanguages = PriorityQueue<DistanceToLanguages>();
-
     for (var curCoord in coordinateToLanguages.keys) {
       var distance = Geolocator.distanceBetween(location.latitude,
           location.longitude, curCoord.latitude, curCoord.longitude);
@@ -185,21 +193,43 @@ class _MyHomePageState extends State<MyHomePage> {
       nearestLanguages.add(DistanceToLanguages(distance, languages));
     }
 
-    // debug print
-    while (nearestLanguages.isNotEmpty) {
-      print(nearestLanguages.removeFirst().languages);
-    }
-
-    // // first audio is system language
-    // var languageCode;
-    // if (localeName == 'fr_CA' || localeName == 'pt_BR') {
-    //   // special case for 'French (Canada)' and 'Portuguese (Brazil)'
-    //   languageCode = localeName;
-    // } else {
-    //   languageCode = localeName.split('_')[0]; // in the form e.g.: 'en_US'
+    // debug print. works!
+    // while (nearestLanguages.isNotEmpty) {
+    //   print(nearestLanguages.removeFirst().languages);
     // }
 
-    return [];
+    // In order list of languages to play
+    List<String?> inOrderLanguages = [];
+
+    // first language is system language
+    String languageCode;
+    if (localeName == 'fr_CA' || localeName == 'pt_BR') {
+      // special case for 'French (Canada)' and 'Portuguese (Brazil)'
+      languageCode = localeName;
+    } else {
+      languageCode = localeName.split('_')[0]; // in the form e.g.: 'en_US'
+    }
+    inOrderLanguages.add(languageCodeToName[languageCode]);
+
+    // Add closest languages first
+    while (nearestLanguages.isNotEmpty) {
+      var curLanguages = nearestLanguages.removeFirst().languages;
+      if (curLanguages != null) {
+        // TODO: really neccesary to check for null?
+        for (var language in curLanguages) {
+          if (!inOrderLanguages.contains(language)) {
+            inOrderLanguages.add(language);
+          }
+        }
+      }
+    }
+
+    // debug check language order. works!
+    for (var element in inOrderLanguages) {
+      print(element);
+    }
+
+    return inOrderLanguages;
   }
 
   @override
