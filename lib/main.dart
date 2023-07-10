464731ce1ef57c97cb8b8acced23deda8435e1b6
Skredder <taylor_juve@outlook.com>
2023-07-10 10:19:44 -0700

    Start of location; codeToLanguage complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'dart:io';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const JustAButton());
@@ -30,33 +31,164 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+// TODO: first sound using device language
+// TODO: following sounds using location
+// Plan: get location; forEach coordinate add pair to priorityQueue;
+// play the first pair's audiofile then the next until empty
 class _MyHomePageState extends State<MyHomePage> {
-  // All supported flutter and text-to-speech languages from code to name
-  // Will add 5 at a time (here and there)
-  // Is a slower LinkedHashMap but this supports better syntax
+  // All Microsoft Translater text-to-speech languages from code to name.
+  // Codes are from Flutter's Platform widget in 'ISO 639-1' format.
+  // Languages are as seen in Microsoft Translater.
+  // Is a slower LinkedHashMap but this supports const.
+  // Doubled checked for accuracy!
   static const Map<String, String> languageCodeToName = {
-    'af': 'Afrikaans',
-    'am': 'Amharic',
-    'ar': 'Arabic',
-    'bn': 'Bangla', // Bengali
-    'bg': 'Bulgarian',
+    'af': 'afrikaans',
+    'am': 'amharic',
+    'ar': 'arabic',
+    'bn': 'bangla', // Bengali
+    'bg': 'bulgarian',
+    // Cantonese (Traditional) missing from codes
+    'ca': 'catalan',
+    'zh': 'chinese', // Simplified and Traditional are same for this
+    'hr': 'croatian',
+    'cs': 'czech',
+    'da': 'danish',
+    'nl': 'dutch',
+    'en': 'english',
+    'et': 'estonian',
+    'fil': 'filipino',
+    'fi': 'finnish',
+    'fr': 'french',
+    'fr_CA': 'french_canada',
+    'gl': 'galician',
+    'de': 'german',
+    'el': 'greek',
+    'gu': 'gujarati',
+    'iw': 'hebrew',
+    'hi': 'hindi',
+    'hu': 'hungarian',
+    'is': 'icelandic',
+    'in': 'indonesian',
+    'ga': 'irish',
+    'it': 'italian',
+    'ja': 'japanese',
+    'kn': 'kannada',
+    'kk': 'kazakh',
+    'km': 'khmer',
+    'ko': 'korean',
+    'lo': 'lao',
+    'lv': 'latvian',
+    'lt': 'lithuanian',
+    'mk': 'macedonian',
+    'ms': 'malay',
+    'ml': 'malayalam',
+    'mt': 'maltese',
+    'mr': 'marathi',
+    'my': 'myanmar',
+    'nb': 'norwegian', // from Bokmal
+    'nn': 'norwegian', // from Nynorsk
+    'ps': 'pashto',
+    'fa': 'persian',
+    'pl': 'polish',
+    'pt_BR': 'portuguese_brazil',
+    'pt': 'portuguese_portugal',
+    'ro': 'romanian',
+    'ru': 'russian',
+    'sk': 'slovak',
+    'sl': 'slovenian',
+    'so': 'somali',
+    'es': 'spanish',
+    'sw': 'swahili',
+    'sv': 'swedish',
+    'ta': 'tamil',
+    'te': 'telugu',
+    'th': 'thai',
+    'tr': 'turkish',
+    'uk': 'ukrainian',
+    'ur': 'urdu',
+    'uz': 'uzbek',
+    'vi': 'vietnamese',
+    'cy': 'welsh',
+    'zu': 'zulu',
+  };
+
+  // TODO: populate this after languageCodeToName
+  // Stores GPS coordinates of capital of country using the list of official
+  // languages (supported languages only)
+  static const Map<Coordinate, List<String>> coordinateToLanguages = {
+    Coordinate(-25.746111, 28.188056): ['Afrikaans'], // South Africa
+    Coordinate(9.016667, 38.75): ['Amharic'], // Ethiopia
+    Coordinate(36.7, 3.216667): ['Arabic'], // Algeria
   };
 
   final AudioPlayer player = AudioPlayer();
   var isPlaying = false; // doesn't start 'til pressed
-  final deviceLanguage = Platform.localeName.split('_')[0];
+  final localeName = Platform.localeName;
 
   _MyHomePageState() {
     // debug print language code
-    print(deviceLanguage);
+    print(localeName);
 
     player.onPlayerComplete.listen((event) {
+      // doesn't trigger on .stop();
       isPlaying = false; // works
     });
+
+    // debug print location
+    _determinePosition().then((position) => print(position));
   }
 
-  // TODO: first sound using Platform language
-  // TODO: following sounds using location
+  // Sample code found at 'https://pub.dev/packages/geolocator'
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
+  // TODO: work in progress!
+  List<String> getAudioFilesInOrder(var locationCoord) {
+    // first audio is system language
+    var languageCode;
+    if (localeName == 'fr_CA' || localeName == 'pt_BR') {
+      // special case for 'French (Canada)' and 'Portuguese (Brazil)'
+      languageCode = localeName;
+    } else {
+      languageCode = localeName.split('_')[0]; // in the form e.g.: 'en_US'
+    }
+
+    return [];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -77,4 +209,11 @@ class _MyHomePageState extends State<MyHomePage> {
       )),
     );
   }
+}
+
+class Coordinate {
+  final double latitude; // Latitude, in degrees
+  final double longitude; // Longitude, in degress
+
+  const Coordinate(this.latitude, this.longitude);
 }
