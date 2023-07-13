cebc7c8c77e823edf1866b8809391892358c5534
Skredder <taylor_juve@outlook.com>
2023-07-13 15:27:57 -0700

    random location used when no location. more files.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
-import 'dart:io';
 import 'package:geolocator/geolocator.dart';
 import 'package:collection/collection.dart';
 
@@ -32,7 +34,6 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-// TODO: general null safety
 class _MyHomePageState extends State<MyHomePage> {
   // All(?) Bing Translater text-to-speech languages from code to name.
   // Codes are from Flutter's Platform widget in 'ISO 639-1' format.
@@ -53,7 +54,6 @@ class _MyHomePageState extends State<MyHomePage> {
     'nl': 'dutch',
     'en': 'english',
     'et': 'estonian',
-    'fil': 'filipino',
     'fi': 'finnish',
     'fr': 'french',
     'fr_CA': 'french_canada',
@@ -135,6 +135,53 @@ class _MyHomePageState extends State<MyHomePage> {
     Coordinate(33.5, 36.3): ['arabic'], // Syria
     Coordinate(36.816667, 10.183333): ['arabic'], // Tunisia
     Coordinate(15.353559, 44.205941): ['arabic'], // Yemen
+    Coordinate(23.763889, 90.388889): ['bangla'], // Bangladesh
+    Coordinate(42.683333, 23.316667): ['bulgarian'], // Bulgaria
+    Coordinate(42.506389, 1.521389): ['catalan'], // Andorra
+    Coordinate(39.916667, 116.383333): ['chinese'], // China
+    Coordinate(25.066667, 121.516667): ['chinese'], // Taiwan
+    Coordinate(1.283333, 103.833333): [
+      'chinese',
+      'malay',
+      'english'
+    ], // Singapore
+    Coordinate(45.8, 16): ['croatian'], // Croatia
+    Coordinate(43.866667, 18.416667): ['croatian'], // B&H
+    Coordinate(50.083333, 14.466667): ['czech'], // Czech Republic
+    Coordinate(55.670094, 12.600247): ['danish'], // Denmark
+    Coordinate(52.366667, 4.883333): ['dutch'], // Netherlands
+    Coordinate(50.85, 4.35): ['dutch', 'french', 'german'], // Belgium
+    Coordinate(5.833333, -55.166667): ['dutch'], // Suriname
+    Coordinate(25.078056, -77.338611): ['english'], // Bahamas
+    Coordinate(13.097778, -59.618333): ['english'], // Barbados
+    Coordinate(17.25, -88.766667): ['english'], // Belize
+    Coordinate(-24.658333, 25.908333): ['english'], // Botswana
+    Coordinate(-3.5, 30): ['french', 'english'], // Burundi
+    Coordinate(3.866667, 11.516667): ['french', 'english'], // Cameroon
+    Coordinate(45.4, -75.666667): ['english', 'french'], // Canada
+    Coordinate(15.3, -61.383333): ['english'], // Domenica
+    Coordinate(-18.166667, 178.45): ['english'], // Fiji
+    Coordinate(13.466667, -16.6): ['english'], // The Gambia
+    Coordinate(5.555, -0.1925): ['english'], // Ghana
+    Coordinate(12.05, -61.75): ['english'], // Grenada
+    Coordinate(6.805833, -58.150833): ['english'], // Guyana
+    Coordinate(28.613889, 77.208333): ['hindi', 'english'], // India
+    Coordinate(53.344167, -6.2675): ['irish', 'english'], // Ireland
+    Coordinate(17.971389, -76.793056): ['english'], // Jamaica
+    Coordinate(-1.266667, 36.8): ['english'], // Kenya
+    Coordinate(6.316667, -10.8): ['english'], // Liberia
+    Coordinate(-13.95, 33.7): ['english'], // Malawi
+    Coordinate(35.9, 14.516667): ['maltese', 'english'], // Malta
+    Coordinate(7.116667, 171.066667): ['english'], // Marshall Islands
+    Coordinate(-20.2, 57.5): ['english', 'french'], // Mauritius
+    Coordinate(6.916667, 158.183333): ['english'], // Micronesia
+    Coordinate(-22.566667, 17.083333): ['english'], // Nambia
+    Coordinate(9.066667, 7.483333): ['english'], // Nigeria
+    Coordinate(33.691667, 73.05): ['english'], // Pakistan
+    Coordinate(-9.478889, 147.149444): ['english'], // Papua New Guinea
+    Coordinate(14.582260, 120.974800): ['english'], // Philippines
+    Coordinate(-1.943889, 30.059444): ['french', 'english'], // Rwanda
+    Coordinate(-13.833333, -171.762222): ['english'], // Samoa
   };
 
   final AudioPlayer player = AudioPlayer();
@@ -144,29 +191,28 @@ class _MyHomePageState extends State<MyHomePage> {
   var isPlaying = false; // doesn't start 'til pressed
 
   _MyHomePageState() {
+    // Be ready to play the nearest languages in order on button press!
+    _getLocation().then((actualLocation) {
+      _getLanguagesInOrder(
+          Coordinate(actualLocation.latitude, actualLocation.longitude));
+    }).catchError((err) {
+      // Not allowed to use device location!
+      // Generate a random location
+      Random random = Random();
+      var randLatitude = random.nextDouble() * 180 - 90;
+      var randLongitude = random.nextDouble() * 180 - 90;
+
+      _getLanguagesInOrder(Coordinate(randLatitude, randLongitude));
+    });
+
+    // When one language audio file completes, play the next (or end)
     player.onPlayerComplete.listen((event) {
       _playNextAudio(); // until last audio
     });
-
-    _determineLocation().then((location) {
-      inOrderLanguages = _getLanguagesInOrder(location);
-    });
   }
 
-  void _playNextAudio() async {
-    if (languagesIndex < inOrderLanguages.length) {
-      print(inOrderLanguages[languagesIndex]); // debug
-      await player.play(AssetSource('${inOrderLanguages[languagesIndex]}.mp3'));
-      languagesIndex++;
-    } else {
-      isPlaying = false; // works
-      languagesIndex = 0; // just in case
-    }
-  }
-
-  // Sample code found at 'https://pub.dev/packages/geolocator''
-  // TODO: Handle errors
-  Future<Position> _determineLocation() async {
+  // Sample code found at 'https://pub.dev/packages/geolocator'
+  Future<Position> _getLocation() async {
     bool serviceEnabled;
     LocationPermission permission;
 
@@ -203,23 +249,20 @@ class _MyHomePageState extends State<MyHomePage> {
     return await Geolocator.getCurrentPosition();
   }
 
-  List<String> _getLanguagesInOrder(Position location) {
+  void _getLanguagesInOrder(Coordinate location) {
+    print(location.latitude); // debug
+    print(location.longitude);
+
     // add the distance to each capital and the languages spoken there
     var nearestLanguages = PriorityQueue<DistanceToLanguages>();
     coordinateToLanguages.forEach((curCoord, languages) {
       var distance = Geolocator.distanceBetween(location.latitude,
           location.longitude, curCoord.latitude, curCoord.longitude);
+
       nearestLanguages.add(DistanceToLanguages(distance, languages));
     });
 
-    // debug print. works so far!
-    // while (nearestLanguages.isNotEmpty) {
-    //   print(nearestLanguages.removeFirst().languages);
-    // }
-
-    // In order list of languages to play
-    List<String> inOrderLanguages = [];
-
+    // --- add languages ---
     // first language is system language
     String languageCode;
     if (localeName == 'fr_CA' || localeName == 'pt_BR') {
@@ -235,7 +278,7 @@ class _MyHomePageState extends State<MyHomePage> {
       inOrderLanguages.add(languageName);
     }
 
-    // Add nearest languages first (no duplicates) and stay in order
+    // Add nearest languages first (no duplicates)
     while (nearestLanguages.isNotEmpty) {
       var curLanguages = nearestLanguages.removeFirst().languages;
 
@@ -245,13 +288,17 @@ class _MyHomePageState extends State<MyHomePage> {
         }
       }
     }
+  }
 
-    // debug check language order. works so far!
-    for (var element in inOrderLanguages) {
-      print(element);
+  void _playNextAudio() async {
+    if (languagesIndex < inOrderLanguages.length) {
+      print(inOrderLanguages[languagesIndex]); // debug
+      await player.play(AssetSource('${inOrderLanguages[languagesIndex]}.mp3'));
+      languagesIndex++;
+    } else {
+      isPlaying = false; // works
+      languagesIndex = 0; // just in case
     }
-
-    return inOrderLanguages;
   }
 
   @override
